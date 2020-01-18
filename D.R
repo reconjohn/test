ae0ee627b62ecca9ad7cf25eca66ee56eb606e52
Yohan Min <min25@uw.edu>
2020-01-17 23:30:29 -0800

    to D

---
+++ D.R
@@ -0,0 +1,17 @@
+#' ---
+#' title: "Yohan in trouble for how to response"
+#' author: "Yohan"
+#' date: "Jan. 17, 2020"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     keep_md: true
+#' ---
+
+#+ r setup, echo= F, warning=F, message=F
+knitr::opts_chunk$set(message = FALSE, warning = FALSE)
+
+
+#' My dear, Sorry, of course I'd like to talk to you, but, if I talked to you by sending a message then I break the rule and the last 3 days that we saved so far, is going to be useless and we gotta start again. That's why I sent at least emogi which confirms that I acknowledge your message. I mean we can talk, but you gotta tell a lie that you don't contact me. If you don't want to lie, yeah, we should not talk until you set up with him after that, we can talk without any burden. In fact I really want to share bible story about Jacob I read today. He was running away from his Brother and it reminded me of you.
+#' 
+#'  Anyway how are you? You know It is really hard, cos you don't want to lie, and you wanna talk. then What should I do? ^^;;
---
+++ D.md
@@ -0,0 +1,21 @@
+---
+title: "Yohan in trouble for how to response"
+author: "Yohan"
+date: "Jan. 17, 2020"
+output:
+  html_document:
+    toc: true
+    keep_md: true
+---
+
+
+
+My dear, Sorry, of course I'd like to talk to you, but, if I talked to you by sending a message then I break the rule and the last 3 days that we saved so far, is going to be useless and we gotta start again. That's why I sent at least emogi which confirms that I acknowledge your message. I mean we can talk, but you gotta tell a lie that you don't contact me. If you don't want to lie, yeah, we should not talk until you set up with him after that, we can talk without any burden. In fact I really want to share bible story about Jacob I read today. He was running away from his Brother and it reminded me of you.
+
+ Anyway how are you? You know It is really hard, cos you don't want to lie, and you wanna talk. then What should I do? ^^;;
+
+---
+title: "D.R"
+author: "Yohan_Min"
+date: "2020-01-17"
+---
